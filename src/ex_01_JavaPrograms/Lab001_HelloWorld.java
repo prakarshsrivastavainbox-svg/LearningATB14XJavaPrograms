6a20b72959c8b960420d19c6d4425864bf7bb2db
Prakarsh Srivastava <prakarshsrivastava.inbox@gmail.com>
2025-09-13 10:42:59 +0530

    Initial commit

---
+++ src/ex_01_JavaPrograms/Lab001_HelloWorld.java
@@ -0,0 +1,8 @@
+package ex_01_JavaPrograms;
+
+public class Lab001_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+       // System.out.println("Hello World!");
+    }
+}
