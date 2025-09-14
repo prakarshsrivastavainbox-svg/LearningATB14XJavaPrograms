753c83855c21e91c354b4ffc4acb6d3328a386f4
Prakarsh Srivastava <prakarshsrivastava.inbox@gmail.com>
2025-09-14 23:24:51 +0530

    Today Learning

---
+++ src/ex_02_JavaPrograms/Lab010_Variables.java
@@ -0,0 +1,19 @@
+package ex_02_JavaPrograms;
+
+public class Lab010_Variables {
+    public static void main(String[] args) {
+        byte pra = 65;
+        short pra1 = 65;
+        byte pra2= 128; // overflowing out of space
+        int enum = 1234; /* this is not possible as enum is keyword, but if we replace enum to Enum then it will work since Java is a
+        //sensitive language*/
+ /*
+ byte is datatype
+  pra is identifier
+  = operator
+  65 is value or lietral
+  */
+
+
+    }
+}
---
+++ src/ex_02_JavaPrograms/Lab011_Boolean.java
@@ -0,0 +1,9 @@
+package ex_02_JavaPrograms;
+
+public class Lab011_Boolean {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.print("a" + "b");
+    }
+}
