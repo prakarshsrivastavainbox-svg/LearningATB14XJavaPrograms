4731f2af1bbacd4883ad929146abeff5a5a1a31a
Prakarsh Srivastava <prakarshsrivastava.inbox@gmail.com>
2025-09-14 11:08:53 +0530

    test

--- src/ex_01_JavaPrograms/Lab001_HelloWorld.java
+++ src/ex_01_JavaPrograms/Lab001_HelloWorld.java
@@ -3,6 +3,5 @@ package ex_01_JavaPrograms;
 public class Lab001_HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
-       // System.out.println("Hello World!");
     }
 }
